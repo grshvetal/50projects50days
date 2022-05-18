237317b1b6d024be575ebaae7fea191c2a4f6541
Shvetal Gamit <72142454+grshvetal@users.noreply.github.com>
2022-05-18 18:21:52 +0530

    add JS for project Drink Water:small glasses behavior

---
+++ projects/drink-water/script.js
@@ -0,0 +1,22 @@
+const liters = document.getElementById('liters')
+const remained = document.getElementById('remained')
+const percentage = document.getElementById('percentage')
+const smallCups = document.querySelectorAll('.cup-small')
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+})
+
+function highlightCups(idx) {
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+}
